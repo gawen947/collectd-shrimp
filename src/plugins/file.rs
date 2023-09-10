bcbd3f6c6c2bd3bcc411c71d156570686c1ab969
David Hauweele <david@hauweele.net>
2023-09-10 19:11:56 +0200

    lint: cargo fmt

--- src/plugins/file.rs
+++ src/plugins/file.rs
@@ -31,10 +31,13 @@ impl plugin::PluginExecImplementation for Settings {
         for target in targets {
             results.push(plugin::PluginResult {
                 time: plugin::now(),
-                value: std::fs::read_to_string(target).unwrap_or_else(|_| {
-                    println!("error: cannot read file '{}'", target);
-                    exit(1);
-                }).trim().to_owned(),
+                value: std::fs::read_to_string(target)
+                    .unwrap_or_else(|_| {
+                        println!("error: cannot read file '{}'", target);
+                        exit(1);
+                    })
+                    .trim()
+                    .to_owned(),
                 target: Some(target),
                 type_instance: None,
             });
